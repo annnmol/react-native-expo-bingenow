7fe9b1850bf1d86e1ec46673cbef6c8f36412000
Anmol Tanwar <tanwaranmol6@gmail.com>
2023-09-17 16:15:26 +0530

    path fixed

---
+++ app/appComponents/cards/threeByFive/ThreeByFiveCard.tsx
@@ -0,0 +1,75 @@
+import { LinearGradient } from "expo-linear-gradient";
+import React, { ReactNode } from "react";
+import {
+  GestureResponderEvent,
+  Image,
+  ImageSourcePropType,
+  StyleProp,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
+import { colors, constants } from "../../../themes";
+import { AppStyles } from "../../../themes/AppStyles";
+import AppFastImage from "../../components/AppFastImage";
+
+interface Props {
+  image: ImageSourcePropType;
+  isActive?: boolean;
+  onPress?: (event: GestureResponderEvent) => void;
+  style?: StyleProp<ViewStyle>;
+  renderSaveIcon?: ReactNode;
+  renderPremiumIcon: ReactNode;
+}
+
+const ThreeByFiveCard: React.FC<Props> = ({
+  image,
+  onPress,
+  isActive = false,
+  style,
+  renderSaveIcon,
+  renderPremiumIcon,
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress} activeOpacity={0.9}>
+      <View style={[styles.container, isActive && styles.active, style]}>
+        <LinearGradient
+          colors={["rgba(0,0,0,0.4)", "transparent", "rgba(0,0,0,0.1)"]}
+          style={AppStyles.backgroundGradient}
+        />
+         <Image
+          source={
+            image
+          }
+          style={styles.icon}
+        />
+      </View>
+      {renderSaveIcon}
+      {renderPremiumIcon}
+    </TouchableOpacity>
+  );
+};
+
+export default ThreeByFiveCard;
+
+const styles = StyleSheet.create({
+  container: {
+    width: constants.windowWidth / 3.8,
+    height: constants.windowHeight / 6,
+    borderRadius: 16,
+    backgroundColor: colors.light600,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    position: "relative",
+  },
+  active: {
+    backgroundColor: colors.bgColor300,
+  },
+  icon: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "stretch",
+  },
+});
---
+++ app/appComponents/cards/threeByFive/ThreeByFiveSection.tsx
@@ -0,0 +1,158 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import { ROUTES_NAMES } from "../../../navigation/Routes";
+import { movieImageUrl500 } from "../../../services/ApiService";
+import { colors, constants, typography } from "../../../themes";
+import { placeholderImage } from "../../../utils";
+import AppText from "../../components/AppText";
+import AppWishlistButton from "../../components/AppWishlistButton";
+import AppExpoIcons from "../../icons/AppExpoIcons";
+import AppIconButton from "../../icons/AppIconButton";
+import ThreeByFiveCard from "./ThreeByFiveCard";
+import AppActivityIndicator from "../../loaders/AppActivityIndicator";
+import AppNoData from "../../components/AppNoData";
+
+interface Props {
+  data: any[];
+  title?: string;
+  searchSlug?: string;
+  isLoading?: boolean;
+}
+
+const ThreeByFiveSection: React.FC<Props> = ({
+  data,
+  title = "Trending",
+  searchSlug,
+  isLoading = false,
+}) => {
+  const navigation = useNavigation<any>();
+
+  const handleSeeALlBtn = () => {
+    if(!searchSlug) return ;
+    navigation.navigate(ROUTES_NAMES.VIEW_ALL_LISTINGS, {
+      id: title,
+      searchSlug: searchSlug ?? title,
+    });
+  };
+  const handleCardClick = (item: any) => {
+    let mediaType = item?.hasOwnProperty("first_air_date") ? "tv" : "movie";
+    navigation.navigate(ROUTES_NAMES.DETAILS, {
+      id: item?.id,
+      mediaType: mediaType,
+    });
+  };
+
+  return (
+    <View style={[styles.container]}>
+      <View style={[styles.infoBox]}>
+        <AppText style={[styles.sectionTitle]}>{title}</AppText>
+        {
+          searchSlug ? (
+
+            <AppIconButton onPress={() => handleSeeALlBtn()}>
+          <AppExpoIcons
+            name={"chevron-right"}
+            size={20}
+            color={colors.dark300}
+            />
+        </AppIconButton>
+            
+          ) : null
+        }
+      </View>
+      {!isLoading ? (
+        <FlatList
+          data={data}
+          initialNumToRender={10}
+          snapToStart={true}
+          snapToAlignment={"center"}
+          ListEmptyComponent={<AppNoData />}
+          renderItem={({ item, index }) => {
+            return (
+              <ThreeByFiveCard
+                image={{
+                  uri: movieImageUrl500(item?.poster_path) ?? placeholderImage,
+                }}
+                onPress={() => handleCardClick(item)}
+                style={[
+                  index === 0 && { marginLeft: constants.paddingHorizontalApp },
+                  index === data?.length - 1 && {
+                    marginRight: constants.paddingHorizontalApp,
+                  },
+                ]}
+                renderPremiumIcon={
+                  item?.vote_average > 7.5 ? (
+                    <AppIconButton
+                      style={[
+                        styles.premiumIconBtn,
+                        index === 0 && {
+                          marginLeft: constants.paddingHorizontalApp,
+                        },
+                      ]}
+                      onPress={() => console.log("")}
+                    >
+                      <AppExpoIcons
+                        name="crown-circle-outline"
+                        color={colors.warning}
+                        size={20}
+                      />
+                    </AppIconButton>
+                  ) : null
+                }
+              />
+            );
+          }}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={() => (
+            <View style={{ backgroundColor: "transparent", width: 16 }} />
+          )}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+        />
+      ) : (
+        <AppActivityIndicator
+          style={[{ height: constants.windowHeight / 6 }]}
+          showText={true}
+        />
+      )}
+    </View>
+  );
+};
+
+export default ThreeByFiveSection;
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 8,
+    gap: 8,
+    position: "relative",
+  },
+
+  infoBox: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: constants.paddingHorizontalApp,
+  },
+  sectionTitle: {
+    fontSize: typography.text1REM,
+    color: colors.dark300,
+    fontWeight: "600",
+    letterSpacing: 0.5,
+  },
+
+  premiumIconBtn: {
+    zIndex: 1,
+    width: 36,
+    height: 36,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+    position: "absolute",
+    left: 0,
+    top: 0,
+  },
+});
